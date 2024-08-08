8d5973927159defc955f123c3485249e21d24f27
AAllahverdi2 <agamaliyevallahverdi2@gmail.com>
2024-08-08 14:51:50 +0400

    router created

---
+++ src/Pages/Admin/AdminRoot.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import AdminHome from './AdminHome'
+import { Outlet } from 'react-router'
+import Footer from '../../Layout/Admin/Footer'
+import Header from '../../Layout/Admin/Header'
+
+const AdminRoot = () => {
+  return (
+    <div>
+        {/* <AdminHome/> */}
+        <Header/>
+        <Outlet/>
+        <Footer/>
+      
+    </div>
+  )
+}
+
+export default AdminRoot
